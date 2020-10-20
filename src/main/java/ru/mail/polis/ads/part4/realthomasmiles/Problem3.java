6b6758f2ebba225a8627c075cb4b5932b7b60301
RealThomasMiles <dmilya2000@gmail.com>
2020-10-20 13:18:36 +0300

    Solved the third problem

---
+++ src/main/java/ru/mail/polis/ads/part4/realthomasmiles/Problem3.java
@@ -0,0 +1,67 @@
+package ru.mail.polis.ads.part4.realthomasmiles;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Problem3 {
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int n1 = in.nextInt();
+        int[] arr1 = new int[n1];
+        for (int i = 0; i < n1; ++i) {
+            arr1[i] = in.nextInt();
+        }
+        int n2 = in.nextInt();
+        int[] arr2 = new int[n2];
+        for (int i = 0; i < n2; ++i) {
+            arr2[i] = in.nextInt();
+        }
+
+        int[][] d = new int[n1 + 1][n2 + 1];
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (arr1[i - 1] == arr2[j - 1]) {
+                    d[i][j] = 1 + d[i - 1][j];
+                } else {
+                    d[i][j] = Math.max(d[i - 1][j], d[i][j - 1]);
+                }
+            }
+        }
+
+        out.println(d[n1][n2]);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
