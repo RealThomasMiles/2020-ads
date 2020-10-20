62161c71d109e4a55d3726f17c08c628e167ec7e
RealThomasMiles <dmilya2000@gmail.com>
2020-10-20 16:01:56 +0300

    Solved the fifth problem

---
+++ src/main/java/ru/mail/polis/ads/part4/realthomasmiles/Problem5.java
@@ -0,0 +1,105 @@
+package ru.mail.polis.ads.part4.realthomasmiles;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Problem5 {
+    private static int counter;
+
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; ++i) {
+            arr[i] = in.nextInt();
+        }
+        counter = 0;
+
+        mergeSort(arr, n);
+
+        out.println(counter);
+    }
+
+    private static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+     
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+     
+        merge(a, l, r, mid, n - mid);
+    }
+
+    private static void merge(int[] a, int[] l, int[] r, int left, int right) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k] = l[i];
+                k++;
+                i++;
+            }
+            else {
+                a[k] = r[j];
+                k++;
+                j++;
+                counter += left - i;
+            }
+        }
+        while (i < left) {
+            a[k] = l[i];
+            k++;
+            i++;
+        }
+        while (j < right) {
+            a[k] = r[j];
+            k++;
+            j++;
+        }
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
