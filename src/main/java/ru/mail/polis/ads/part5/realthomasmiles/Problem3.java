723912df2f9b0cb5f15b30a206a376ed7747af7d
RealThomasMiles <dmilya2000@gmail.com>
2020-10-20 20:45:40 +0300

    Solved the third problem

---
+++ src/main/java/ru/mail/polis/ads/part5/realthomasmiles/Problem3.java
@@ -0,0 +1,60 @@
+package ru.mail.polis.ads.part5.realthomasmiles;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Problem3 {
+    private static void solve(final FastScanner in, final PrintWriter out) {
+        long w = in.nextInt();
+        long h = in.nextInt();
+        long n = in.nextInt();
+
+        long l = Math.max(w, h);
+        long r = l * n;
+        while (l < r) {
+            long m = (l + r) / 2;
+            long count = (m / h) * (m / w);
+            if (n <= count) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        out.println(l);
+    }
+
+    private static class FastScanner {
+        private final BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        FastScanner(final InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+        }
+
+        String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public static void main(final String[] arg) {
+        final FastScanner in = new FastScanner(System.in);
+        try (PrintWriter out = new PrintWriter(System.out)) {
+            solve(in, out);
+        }
+    }
+}
