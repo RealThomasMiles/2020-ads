db54e635b929112b7f69ac39b63be4f528ddfffb
RealThomasMiles <dmilya2000@gmail.com>
2020-11-23 14:50:36 +0300

    Changed casting

--- src/main/java/ru/mail/polis/ads/hash/HashTableClass.java
+++ src/main/java/ru/mail/polis/ads/hash/HashTableClass.java
@@ -4,12 +4,12 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class HashTableClass<Key, Value> implements HashTable<Key, Value> {
-    private static class Element<Key, Value> {
-        private final Key key;
-        private Value value;
-        private Element<Key, Value> next;
+    private static class Element<K, V> {
+        private final K key;
+        private V value;
+        private Element<K, V> next;
 
-        public Element(Key key, Value value, Element<Key, Value> next) {
+        public Element(K key, V value, Element<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
@@ -20,18 +20,19 @@ public class HashTableClass<Key, Value> implements HashTable<Key, Value> {
     private int capacity;
     private float loadFactor;
     private int currentPrime;
-    private Object[] elements;
+    private Element<Key, Value>[] elements;
     private final int[] prime = new int[] {17, 37, 67, 131, 263, 541, 1087, 2179, 4363, 8731, 17467, 34939, 69899, 139801, 279607, 559217, 1118419};
 
     private int hash(Key key) {
         return (key.hashCode() & 0x7fffffff) % capacity;
     }
 
+    @SuppressWarnings("unchecked")
     private void resize() {
         currentPrime++;
         capacity = prime[currentPrime];
         Object[] temp = elements;
-        elements = new Object[capacity];
+        elements = (Element<Key, Value>[]) new Element[capacity];
         for (Object o : temp) {
             @SuppressWarnings("unchecked")
             Element<Key, Value> tempE = (Element<Key, Value>) o;
@@ -39,19 +40,19 @@ public class HashTableClass<Key, Value> implements HashTable<Key, Value> {
         }
     }
 
+    @SuppressWarnings("unchecked")
     public HashTableClass() {
         size = 0;
         currentPrime = 0;
         capacity = prime[currentPrime];
         loadFactor = 0;
-        elements = new Element[capacity];
+        elements = (Element<Key, Value>[]) new Element[capacity];
     }
 
     @Override
     public @Nullable Value get(@NotNull Key key) {
         int hash = hash(key);
-        @SuppressWarnings("unchecked")
-        Element<Key, Value> root = (Element<Key, Value>) elements[hash];
+        Element<Key, Value> root = elements[hash];
 
         while (root != null) {
             if (root.key.equals(key)) {
@@ -66,8 +67,7 @@ public class HashTableClass<Key, Value> implements HashTable<Key, Value> {
     @Override
     public boolean containsKey(@NotNull Key key) {
         int hash = hash(key);
-        @SuppressWarnings("unchecked")
-        Element<Key, Value> root = (Element<Key, Value>) elements[hash];
+        Element<Key, Value> root = elements[hash];
 
         while (root != null) {
             if (root.key.equals(key)) {
@@ -80,11 +80,10 @@ public class HashTableClass<Key, Value> implements HashTable<Key, Value> {
     }
 
     @Override
-    @SuppressWarnings("unchecked")
     public void put(@NotNull Key key, @NotNull Value value) {
         int hash = hash(key);
 
-        Element<Key, Value> root = (Element<Key, Value>) elements[hash];
+        Element<Key, Value> root = elements[hash];
 
         while (root != null) {
             if (root.key.equals(key)) {
@@ -96,7 +95,7 @@ public class HashTableClass<Key, Value> implements HashTable<Key, Value> {
 
         size++;
         loadFactor = size / (float) capacity;
-        root = (Element<Key, Value>) elements[hash];
+        root = elements[hash];
         Element<Key, Value> newElement = new Element<>(key, value, root);
         elements[hash] = newElement;
 
@@ -107,8 +106,7 @@ public class HashTableClass<Key, Value> implements HashTable<Key, Value> {
     public @Nullable Value remove(@NotNull Key key) {
         if (!containsKey(key)) return null;
         int hash = hash(key);
-        @SuppressWarnings("unchecked")
-        Element<Key, Value> root = (Element<Key, Value>) elements[hash];
+        Element<Key, Value> root = elements[hash];
 
         Element<Key, Value> prev = null;
         while (root != null) {
